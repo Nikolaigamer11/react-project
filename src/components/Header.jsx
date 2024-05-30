8514b73706b4cfba2b3a9dbba2bf93f38db966b0
Nikolaigamer11 <ankurpathak005@gmail.com>
2024-05-30 17:09:08 +0530

    modified:   index.html 	modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li class= "text-"><Link to="/">Home</Link></li>
-                    <li><Link to="/Shop">Shop</Link></li>
-                    <li><Link to="/cart">Cart</Link></li>
-                </ul>
-            </nav>
+            <nav className="bg-gray-800 text-white p-4 sticky top-0 z-10">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-center">
+                    <NavLink to="/" className="text-lg font-semibold">Clean Bin</NavLink>
+                    <ul className="ml-4 flex space-x-4">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/shop">Shop</NavLink></li>
+                        <li><NavLink to="/about">About</NavLink></li>
+                    </ul>
+                </div>
+                <div className="flex items-center">
+                    <input type="text" placeholder="Search" className="mr-4 p-2 rounded-md border border-gray-400" />
+                    <button className="mr-4">
+                        {/* Cart icon and logic here */}
+                    </button>
+                    <button>
+                        
+                    </button>
+                </div>
+            </div>
+        </nav>
         </header>
     );
 };
